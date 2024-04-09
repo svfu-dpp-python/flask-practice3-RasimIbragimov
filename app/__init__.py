1efa204b3962b0bf2be5b19624b3faafbc51ec8f
RasimIbragimov <planet_saturn2001@mail.ru>
2024-04-09 16:48:34 +0900

    Задание 1

---
+++ app/__init__.py
@@ -0,0 +1,17 @@
+from flask import Flask
+
+from app import views
+from app.database import db, migrate
+
+
+def create_app():
+    app = Flask(__name__)
+
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.db"
+    db.init_app(app)
+    migrate.init_app(app, db)
+
+    app.add_url_rule("/", view_func=views.index_page)
+
+    return app
+
